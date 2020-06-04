b62781ba8a2d75920c6460ed00f6615abd899628
JeasonLau <b1361974998@gmail.com>
2020-06-04 17:11:44 +0800

    feat: init commit
    初始化提交electron-vue脚手架

---
+++ src/renderer/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <h1>content is {{result}}</h1>
+        <input v-model="url">
+        <a-button type="primary" @click="test()"></a-button>
+        <!--
+        <router-view></router-view>
+        -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'coolapk',
+        data: function () {
+            let result = 1
+            let url = 'https://www.baidu.com'
+            return {
+                result,
+                url
+            }
+        },
+        methods: {
+            test() {
+                this.$http.get(this.url).then(response => {
+                    this.result = response.data
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+    /* CSS */
+</style>
