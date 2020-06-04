f37a681c7df9263801ae1b399600d5ca95a3f318
JeasonLau <b1361974998@gmail.com>
2020-06-05 00:15:26 +0800

    feat: support entities
    增加了对头图走马灯的渲染

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,7 +1,31 @@
 <template>
     <div id="app">
         <meta name="referrer" content="no-referrer"/>
-        <router-view></router-view>
+        <a-layout>
+            <a-layout-sider>
+                <a-menu
+                        :default-selected-keys="['1']"
+                        :default-open-keys="['sub1']"
+                        mode="inline"
+                        theme="dark"
+                >
+                    <a-menu-item key="1">
+                        <a-icon type="mail"/>
+                        Navigation One
+                    </a-menu-item>
+                    <a-menu-item key="2">
+                        <a-icon type="calendar"/>
+                        Navigation Two
+                    </a-menu-item>
+                </a-menu>
+            </a-layout-sider>
+            <a-layout>
+                <a-layout-content>
+                    <router-view></router-view>
+                </a-layout-content>
+                <a-layout-footer>Footer</a-layout-footer>
+            </a-layout>
+        </a-layout>
     </div>
 </template>
 
--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        <div v-for="item in result['data'][0]['entities']">
-            <img v-bind:src="item.pic"/>
-        </div>
+        <a-carousel autoplay>
+            <div v-for="item in result['data'][0]['entities']">
+                <div>
+                    <img v-bind:src="item.pic" width="100%" height="10%"/>
+                </div>
+            </div>
+        </a-carousel>
     </div>
 </template>
 
@@ -12,7 +16,7 @@
     const request = require('request')
     export default {
         data: function () {
-            let result = 1
+            let result='1'
             return {
                 result
             }
@@ -24,39 +28,44 @@
                 let time = Date.parse(new Date()) / 1000
                 let tmp1 = '0x' + time.toString(16)
                 let tmp2 = md5(time.toString())
-                console.log(tmp2)
                 let tmp3 = 'token://com.coolapk.market/c67ef5943784d09750dcfbb31020f0ab?' + tmp2 + '$' + device_ID + '&com.coolapk.market'
                 let tmp4 = md5(btoa(tmp3))
                 let token = tmp4 + device_ID + tmp1
                 return token
+            },
+            getJson(url) {
+                let token = this.getToken()
+                let options = {
+                    headers: {
+                        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 9; MI 8 SE MIUI/9.5.9) (#Build; Xiaomi; MI 8 SE; PKQ1.181121.001; 9) +CoolMarket/9.2.2-1905301",
+                        "X-App-Id": "com.coolapk.market",
+                        "X-Requested-With": "XMLHttpRequest",
+                        "X-Sdk-Int": "28",
+                        "X-Sdk-Locale": "zh-CN",
+                        "X-Api-Version": "9",
+                        "X-App-Version": "9.2.2",
+                        "X-App-Code": "1903501",
+                        "X-App-Device": "QRTBCOgkUTgsTat9WYphFI7kWbvFWaYByO1YjOCdjOxAjOxEkOFJjODlDI7ATNxMjM5MTOxcjMwAjN0AyOxEjNwgDNxITM2kDMzcTOgsTZzkTZlJ2MwUDNhJ2MyYzM",
+                        "Host": "api.coolapk.com",
+                        "X-Dark-Mode": "0",
+                        "X-App-Token": token,
+                    },
+                    url: url,
+                }
+                request.get(options, (error, response, body) => {
+                        if (!error) {
+                            this.result= JSON.parse(body)
+                        } else {
+                            console.log('error!')
+                            this.result='error!'
+                        }
+                    }
+                )
             }
         },
         created() {
-            let token = this.getToken()
-            let options = {
-                headers: {
-                    "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 9; MI 8 SE MIUI/9.5.9) (#Build; Xiaomi; MI 8 SE; PKQ1.181121.001; 9) +CoolMarket/9.2.2-1905301",
-                    "X-App-Id": "com.coolapk.market",
-                    "X-Requested-With": "XMLHttpRequest",
-                    "X-Sdk-Int": "28",
-                    "X-Sdk-Locale": "zh-CN",
-                    "X-Api-Version": "9",
-                    "X-App-Version": "9.2.2",
-                    "X-App-Code": "1903501",
-                    "X-App-Device": "QRTBCOgkUTgsTat9WYphFI7kWbvFWaYByO1YjOCdjOxAjOxEkOFJjODlDI7ATNxMjM5MTOxcjMwAjN0AyOxEjNwgDNxITM2kDMzcTOgsTZzkTZlJ2MwUDNhJ2MyYzM",
-                    "Host": "api.coolapk.com",
-                    "X-Dark-Mode": "0",
-                    "X-App-Token": token,
-                },
-                url: 'https://api.coolapk.com/v6/main/indexV8?page=1',
-            }
-            request.get(options, (error, response, body) => {
-                if (!error) {
-                    this.result = JSON.parse(body)
-                } else {
-                    this.result = 'error!'
-                }
-            })
+            this.getJson('https://api.coolapk.com/v6/main/indexV8?page=1')
         }
     }
+
 </script>
